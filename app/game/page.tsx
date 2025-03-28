d6d243f29a97d9e04c907cf9b276756467ee69ae
Miki <90838632+3miki@users.noreply.github.com>
2025-03-28 23:09:53 +0100

    refactor: improve button location and background design

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -12,45 +12,51 @@ type Question = {
   url: string;
 };
 
+
 // question component
 export const QuizQuestion = ({
   question,
   setQuizAnswer,
 }: {
   question: Question;
-  setQuizAnswer: React.Dispatch<React.SetStateAction<boolean>>;
-  // incrementQuiz: () => void;
+  setQuizAnswer: React.Dispatch<React.SetStateAction<boolean>>; // type definition for o bject
 }) => {
-  console.log("question", question);
+  console.log("question", question.question);
 
   // for testing fetching quiz and image from DB
   return (
-    <div className="flex flex-col justify-between items-center p-4 rounded-2xl gap-4">
-      <div className="text-orange-700 bg-slate-200/80 p-4 rounded-2xl">
-        <h3 className="text-lg font-bold text-center">
-          Question: {question.question}
+    <div className="flex flex-col justify-between items-center p-4 rounded-2xl gap-4 bg-orange-100/80 w-[700px] h-auto">
+      <div className="h-[100px] items-center text-orange-700 p-4 rounded-2xl">
+        <h3 className="text-lg font-semibold text-center underline">
+          Question
+        </h3>
+        <h3 className="text-lg font-semibold text-center">
+          {question.question}
         </h3>
       </div>
+      <div>
+        <button
+          onClick={() => {
+            console.log("Answer button clicked");
+            setQuizAnswer(true);
+          }}
+          type="submit"
+          className="w-36 rounded-md bg-orange-500 p-2 text-white hover:bg-orange-600 focus:outline-none"
+        >
+          Reveal Answer
+        </button>
+      </div>
       <div
-        className="z-10 w-full flex justify-center"
+        className="z-10 w-full flex justify-center p-2"
         style={{ maxHeight: "60vh", width: "auto" }}
       >
         <Image
           src={question.url}
           alt={`Image for quiz ${question.id}`}
-          width={500} // Adjust as needed
-          height={400} // Adjust as needed
+          width={500}
+          height={400}
           style={{ objectFit: "contain", maxHeight: "100%" }}
         />
-      </div>
-      <div>
-        <button
-          onClick={() => setQuizAnswer(true)}
-          type="submit"
-          className="w-36 rounded-md bg-orange-500 p-2 text-white hover:bg-orange-600 border-2 border-orange-500 focus:outline-none"
-        >
-          See Answer
-        </button>
       </div>
     </div>
   );
@@ -66,75 +72,76 @@ export const QuizAnswer = ({
   setQuizAnswer: React.Dispatch<React.SetStateAction<boolean>>;
   incrementQuiz: () => void;
 }) => {
-  console.log("question", question);
+  console.log("answer", question.answer);
   return (
-    <div className="flex flex-col justify-between items-center p-4 rounded-2xl gap-4">
-      <div className="text-orange-700 bg-slate-200/80 p-4 rounded-2xl">
-        <h3 className="text-lg font-bold text-center">
-          Answer: {question.answer}
-        </h3>
+    <div className="flex flex-col justify-between items-center p-4 rounded-2xl gap-4 bg-orange-200/80 w-[700px] h-auto">
+      <div className="h-[100px] text-orange-700 p-4 rounded-2xl">
+        <h3 className="text-lg font-semibold text-center underline">Answer</h3>
+        <h3 className="text-lg font-semibold text-center">{question.answer}</h3>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            console.log("Quiz button clicked");
+            setQuizAnswer(false);
+            incrementQuiz();
+          }}
+          type="submit"
+          className="w-36 rounded-md bg-orange-500 p-2 text-white hover:bg-orange-600 focus:outline-none"
+        >
+          Next Quiz
+        </button>
       </div>
       <div
-        className="z-10 w-full flex justify-center"
+        className="z-10 w-full flex justify-center p-2"
         style={{ maxHeight: "60vh", width: "auto" }}
       >
         <Image
           src={question.url}
           alt={`Image for quiz ${question.id}`}
-          width={500} // Adjust as needed
-          height={400} // Adjust as needed
-          style={{ objectFit: "contain", maxHeight: "100%" }}
+          width={500}
+          height={400}
+          style={{ objectFit: "contain", maxHeight: "100%", height: "auto" }}
         />
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            console.log("clicked");
-            setQuizAnswer(false);
-            incrementQuiz();
-          }}
-          type="submit"
-          className="w-36 rounded-md bg-orange-500 p-2 text-white hover:bg-orange-600 border-2 border-orange-500 focus:outline-none"
-        >
-          Next Quiz
-        </button>
       </div>
     </div>
   );
 };
 
 export default function Game() {
+  const [fetchError, setFetchError] = useState<string | null>(null);
+  const [quiz, setQuiz] = useState<Question[]>([]);
+
   // check if quizCode=${} is in the url, if it is, render the quiz page component. Otherwise render the home page component
 
-  // test supabase connection
-  // console.log("superbase", supabase);
-  signInWithEmail();
-
-  // increment quiz
+  // increment quiz,
   const incrementQuiz = () => {
     setCurrentIndex((prev) => (prev + 1) % quiz.length);
   };
 
-  const [fetchError, setFetchError] = useState<string | null>(null);
-  const [quiz, setQuiz] = useState<Question[]>([]);
   // const [quizId, setQuizId] = useState<number | null>(1);
 
   useEffect(() => {
+    // test supabase connection
+    // console.log("superbase", supabase);
+
+    // sign in for supabase
+    signInWithEmail();
+    console.log("signed in");
     // create async function
     const fetchQuiz = async () => {
       const { data: fetchedData, error: fetchedError } = await supabase
         .from("quiz")
         .select();
       // .eq("id", 2);
-      console.log("text", fetchedData);
+      console.log("fetched quiz data", fetchedData);
       // .select () to get all the data from the table
       console.log(fetchedError);
       if (fetchedError) {
         setFetchError("Could not fetch quizzes");
         setQuiz([]);
         // console.error(fetchedError)
-      }
-      if (fetchedData) {
+      } else if (fetchedData) {
         setQuiz(fetchedData); // Store the whole array
         setFetchError(null);
       }
@@ -147,7 +154,7 @@ export default function Game() {
     // });
   }, []);
 
-  console.log("render", quiz);
+  // console.log("render", quiz);
 
   // const onClick = async () => {
   //   console.log("clicked");
@@ -178,7 +185,7 @@ export default function Game() {
               />
             ) : undefined}
             {currentIndex === quiz.length - 1 && showAnswer && (
-              <div className="text-center gap-2">
+              <div className="text-center gap-2 py-4">
                 <h2 className="text-2xl font-bold">
                   Congratulations! You've completed the AI quizzes.
                 </h2>
@@ -191,7 +198,7 @@ export default function Game() {
                     type="submit"
                     className="w-36 rounded-md bg-orange-500 p-2 text-white hover:bg-orange-600 border-2  border-orange-500 focus:outline-none"
                   >
-                    Start Again
+                    Start New Game
                   </button>
                 </div>
               </div>
